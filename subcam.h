20703fe7532f36230cace0703cbb84dc9b76cc2b
Uekkky <48573833+Uekkky@users.noreply.github.com>
2019-04-01 15:54:56 +0900

    受信～送信
    受信～送信

--- subcam.h
+++ subcam.h
@@ -12,7 +12,7 @@ class SubCam : public QObject
 
 public:
     explicit SubCam(QObject *parent = nullptr, const QString dest = "192.168.0.10");
-    void recvMainCamData(const LensInfo mainCam);
+    void recvMainCamData(const LensInfo mainCam, const Location TagetPosi);
 
 signals:
 
@@ -24,10 +24,15 @@ private:
     LensInfo _subCam;
     int _angle; //TODO:型修正（適切な型が分からなかったのでとりあえず全部int（六車））
     QByteArray _cmd;
+    Location   _TargetPosi;
+    Location   _SubcamPosi;
+    unsigned int _subcam_AWpan;
 
     bool calcSubCamAngle();
     bool calcSubCamPTZ();
+    bool calcSubCamPosi();
     QByteArray createCmdString();
+    bool num2ascii(unsigned int num, QByteArray &array, int length);
 };
 
 #endif // SUBCAM_H
